8770426dcb69e638541486261dd3aed5da3c589a
Nessiah919 <nesmith.charlesii@gmail.com>
2021-06-04 19:41:22 -0400

    created models 'Collection' and 'Flashcard'in flashcards app of Flashcard_App proj

---
+++ Flashcard_App/flashcards/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Collection(models.Model):
+    title = models.CharField(max_length=50, default='<unknown>')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Flashcard(models.Model):
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=50)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
