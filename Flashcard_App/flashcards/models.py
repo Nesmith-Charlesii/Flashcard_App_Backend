5bafd0470b26f9812b613087b9feb2f4899f6d2f
Nessiah919 <nesmith.charlesii@gmail.com>
2021-06-04 19:44:11 -0400

    added 'created_at' and 'updated_at' model fields to Flashcard model in flashcards app

--- Flashcard_App/flashcards/models.py
+++ Flashcard_App/flashcards/models.py
@@ -12,3 +12,5 @@ class Flashcard(models.Model):
     question = models.CharField(max_length=100)
     answer = models.CharField(max_length=50)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
